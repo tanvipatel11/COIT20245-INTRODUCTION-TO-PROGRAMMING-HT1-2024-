bf5f490421fef3ae85086f7adb4f6f41e6c687c7
tanvipatel11 <162547136+tanvipatel11@users.noreply.github.com>
2024-05-07 19:53:21 +1000

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,1 +1,24 @@
+def display_menu():
+    print("\nCOIT20245 - Assignment 2")
+    print("\nMenu : ")
+    print("a. Help Menu")
+    print("b. Exit")
 
+def main():
+
+    display_menu()
+
+    while True:
+        user_input = input("wildlife > ")
+
+        if user_input.lower() == "help":
+            display_menu()
+        elif user_input.lower() == "exit":
+            print("Exiting the program.")
+            break
+        else:
+            print("Error: Invalid command. Please enter 'help' or 'exit'.")
+
+
+if __name__ == "__main__":
+    main()
