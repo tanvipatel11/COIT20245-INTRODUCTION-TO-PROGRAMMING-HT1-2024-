d5cc499fc2e72530ac6149b067aeabf171f0981b
tanvipatel11 <162547136+tanvipatel11@users.noreply.github.com>
2024-05-07 19:54:38 +1000

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,1 +1,15 @@
+def search_species(city):
+    return [
+        {"Species":{"TaxonID":1039,"AcceptedCommonName":"dolphin","PestStatus":"Nil"}},
+        {"Species":{"TaxonID":1040,"AcceptedCommonName":"snake","PestStatus":"Venomous"}}
+    ]
+
+def display_species(species_list):
+
+    print("\nDetail of species : \n")
+    for species in species_list:
+        print(
+            "TaxonID : " + str(species["Species"]["TaxonID"]) + "\n"
+            "Name of the species : " + species["Species"]["AcceptedCommonName"] + "\n"
+            "Pest Status : " + species["Species"]["PestStatus"] + "\n")
 
